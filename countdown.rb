845fb4c4ec4f2d34e8010559b0c9b5a14129a6c1
Sarah Williams <srwilliams8678@gmail.com>
2019-04-17 19:48:57 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,10 @@
 #write your code here
 
-def countdown (x)
-   x = 10
-   while x > 0
-    puts "#{x} SECOND(S)!"
-    x -= 1
+def countdown (count)
+   count = 10
+   while count > 0
+    puts "#{count} SECOND(S)!"
+    count -= 1
   if x == 0
     puts "HAPPY NEW YEAR!"
     end
