6363ce18c98444510ec3bf0672f9bc770b8595ab
Sarah Williams <srwilliams8678@gmail.com>
2019-04-17 19:51:22 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,14 +9,12 @@ def countdown (count)
  return "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep
-  x = 10
-   while x > 0
-    puts "#{x} SECOND(S)!"
-    x -= 1
+def countdown_with_sleep (count)
+  count = 10
+   while count > 0
+    puts "#{count} SECOND(S)!"
+    count -= 1
     sleep 1
-  if x == 0
-    puts "HAPPY NEW YEAR!"
     end
-  end
+ return "HAPPY NEW YEAR!"
 end
